206049960ca03fcf2bacfc8a00855095bba584f9
claudio.silva <claudio.silva@elements.nl>
2022-09-01 20:59:48 +0200

    add pwa support, improve bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
+
+import manifest from "./manifest.json";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      manifest,
+      includeAssets: ["favicon.ico", "favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
   server: { port: 3000 },
   build: {
     chunkSizeWarningLimit: 1500,
