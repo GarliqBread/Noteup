db46447158fceaf56036d8ff4a88fef037778479
Lucas <lluc23@hotmail.com>
2023-03-24 23:11:32 +0100

    add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
-
+import "./style.css";
 
 import { ThemeWrapper } from "@/components/ThemeWrapper";
 
